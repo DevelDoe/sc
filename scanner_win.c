01bf220de02292a88d6696573dc7cc1a85acdebd
Morgan A Ray <andreeray@live.com>
2025-03-27 23:19:03 +0100

    updating watchdog

--- scanner_win.c
+++ scanner_win.c
@@ -476,7 +476,7 @@ static int finnhub_callback(struct lws *wsi, enum lws_callback_reasons reason, v
 
     switch (reason) {
         case LWS_CALLBACK_CLIENT_ESTABLISHED:
-            LOG_WS("Connected to FINNHUB\n");  // test
+            LOG_WS("Connected to FINNHUB\n");  // testing
             lws_callback_on_writable(wsi);
             break;
 
